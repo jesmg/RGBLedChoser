8a7751f2ab2cac70c5aacaef338e80e5fcccf77a
Jesús Marín <contacto@jesusmg.me>
2015-05-08 01:31:16 +0200

    First commit

---
+++ RGBLed/Arduino Files/WindowsForms_to_RGB.ino
@@ -0,0 +1,46 @@
+// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
+// released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
+
+#include <Adafruit_NeoPixel.h>
+#include <avr/power.h>
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1
+#define PIN            6
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      1
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+// example for more information on possible values.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+int delayval = 500; // delay for half a second
+
+void setup() {
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+#if defined (__AVR_ATtiny85__)
+  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+#endif
+  // End of trinket special code
+  
+
+  pixels.begin(); // This initializes the NeoPixel library.
+        Serial.begin(9600);
+}
+
+String color=0;
+
+void loop() {
+      if(Serial.available()) {
+              int i = 0;
+              while (i<9) {
+                color += Serial.read();
+                i++;
+              }
+              pixels.setPixelColor(0, pixels.Color(color.substring(0, 3).toInt(), color.substring(3, 6).toInt(), color.substring(6, 9).toInt()));
+              pixels.show();
+      }
+
+}
---
+++ RGBLed/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace RGBLed
+{
+    public partial class Form1 : Form
+    {
+        SerialPort serial;
+        public Form1()
+        {
+            InitializeComponent();
+            serial = new SerialPort();
+            serial.PortName = "COM5";
+            serial.BaudRate = 9600;
+            serial.Open();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ColorDialog color = new ColorDialog();
+            color.AllowFullOpen = true;
+            color.AnyColor = true;
+            color.ShowDialog();
+            serial.Write(color.Color.R.ToString()+color.Color.G.ToString()+color.Color.B.ToString());
+        }
+    }
+}
